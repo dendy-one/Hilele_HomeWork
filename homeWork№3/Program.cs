b721790c067b6ab8d4166eb431e671eac7cb5d91
dendy-one <danya.homon0202@gmail.com>
2024-05-28 09:56:08 +0200

    Добавьте файлы проекта.

---
+++ homeWork№3/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            //int studentGrades1 = Convert.ToInt32(Console.ReadLine());
+            //int studentGrades2 = Convert.ToInt32(Console.ReadLine());
+            //int studentGrades3 = Convert.ToInt32(Console.ReadLine());
+            //int studentGrades4 = Convert.ToInt32(Console.ReadLine());
+            //int studentGrades5 = Convert.ToInt32(Console.ReadLine());
+            //int Grades = studentGrades1 + studentGrades2 + studentGrades3 + studentGrades4 + studentGrades5;
+
+            //if (Grades / 5 > 3)
+            //{
+            //    Console.WriteLine("допущен до екзаменна");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("не допущений");
+            //}
+            //Console.ReadKey();
+
+
+
+            //2
+            //var num = Convert.ToDecimal(Console.ReadLine());
+            //if (num % 2 == 0)
+            //{
+            //    num = num * 3;
+            //    Console.WriteLine(num);
+            //}
+
+            //else
+            //{
+            //    num = num / 2;
+            //    Console.WriteLine(num);
+            //}
+            //Console.ReadKey();
+
+
+
+
+            //3
+            //Console.WriteLine("Перше число");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Друге число");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Операція (+,-,*,/)");
+            //char operation = Convert.ToChar(Console.ReadLine());
+            //if (operation == '+')
+            //{
+            //    int num3 = num1 + num2;
+            //    Console.WriteLine($"{num1}+{num2}={num3}");
+            //}
+            //else if (operation == '-')
+            //{
+            //    int num3 = num1 - num2;
+
+            //    Console.WriteLine($"{num1}-{num2}={num3}");
+            //}
+
+            //else if (operation == '*')
+            //{
+            //    int num3 = num1 * num2;
+
+            //    Console.WriteLine($"{num1}*{num2}={num3}");
+            //}
+
+            //else if (operation == '/')
+            //{
+            //    int num3 = num1 / num2;
+
+            //    Console.WriteLine($"{num1}/{num2}={num3}");
+            //}
+            //Console.ReadKey();
+
+
+        }
+    }
+}
