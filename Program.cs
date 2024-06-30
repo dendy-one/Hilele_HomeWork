3fcec14393fb19578205fb9612c1066179bd6222
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-06-30 19:34:15 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,56 +4,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HomeWork7
+namespace homeWork8
 {
     internal class Program
     {
+        static List<string> taskList = new List<string>();
+
         static void Main(string[] args)
         {
-            try
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            while (true)
             {
-                //масив чисел
-                int[] num = new int[30];
-                Random ran = new Random();
+                Console.WriteLine("Оберіть дію:");
+                Console.WriteLine("1. Додати завдання");
+                Console.WriteLine("2. Змінити завдання");
+                Console.WriteLine("3. Видалити завдання");
+                Console.WriteLine("4. Переглянути завдання");
+                Console.WriteLine("5. Вийти");
 
-                //цикл створює рандомні числа від 0 до 101
-                for (int i = 0; i < num.Length; i++)
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
                 {
-                    num[i] = ran.Next(0, 101);
+                    case 1:
+                        AddTask();
+                        break;
+                    case 2:
+                        ChangeTask();
+                        break;
+                    case 3:
+                        RemoveTask();
+                        break;
+                    case 4:
+                        ViewTasks();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Недійсний вибір. Спробуйте ще раз.");
+                        break;
                 }
-
-                // коли виповнився нижній цикл, цей цикл вставляє мінімальну цифру і так ще 30 раз поки не дойде до кінця
-                for (int i = 0; i < num.Length - 1; i++)
-                {
-                    //продивляється кожну цифру масиву і шукає мінімальну
-                    for (int j = 0; j < num.Length - i - 1; j++)
-                    {
-
-                        if (num[j] > num[j + 1])
-                        {
-
-                            int temp = num[j];
-                            num[j] = num[j + 1];
-                            num[j + 1] = temp;
-                        }
-                    }
-                }
-
-
-                Console.WriteLine("просортерованые числа:");
-                foreach (int number in num)
-                {
-                    Console.Write(number + " ");
-                }
-                Console.ReadKey();
             }
+        }
 
-            catch (Exception) 
+        static void AddTask()
+        {
+            Console.Write("Введіть завдання: ");
+            string task = Console.ReadLine();
+            taskList.Add(task);
+            Console.WriteLine($"Завдання '{task}' додано до списку.");
+        }
+
+        static void ChangeTask()
+        {
+            Console.Write("Введіть індекс завдання, яке хочете змінити: ");
+            int index = int.Parse(Console.ReadLine());
+
+            if (index >= 0 && index < taskList.Count)
             {
-                Console.WriteLine("ошибка выполнения сортировки");
-                Console.ReadKey();
-                
+                Console.Write("Введіть нове завдання: ");
+                string newTask = Console.ReadLine();
+                taskList[index] = newTask;
+                Console.WriteLine($"Завдання з індексом {index} змінено на '{newTask}'.");
+            }
+            else
+            {
+                Console.WriteLine("Недійсний індекс завдання.");
+            }
+        }
+
+        static void RemoveTask()
+        {
+            Console.Write("Введіть індекс завдання, яке хочете видалити: ");
+            int index = int.Parse(Console.ReadLine());
+
+            if (index >= 0 && index < taskList.Count)
+            {
+                taskList.RemoveAt(index);
+                Console.WriteLine($"Завдання з індексом {index} видалено.");
+            }
+            else
+            {
+                Console.WriteLine("Недійсний індекс завдання.");
+            }
+        }
+
+        static void ViewTasks()
+        {
+            Console.WriteLine("Список завдань:");
+            for (int i = 0; i < taskList.Count; i++)
+            {
+                Console.WriteLine($"{i}. {taskList[i]}");
             }
         }
     }
+        
+    
 }
