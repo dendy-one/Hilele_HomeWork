a199f85259cc3f7c812367a65e18dc8824cd9aaa
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-07-05 19:54:39 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,50 +1,125 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System;
+using System.Threading.Tasks;
 
-class Program
+namespace HomeWork10
 {
-    static void Main()
-    {
-        Console.OutputEncoding = Encoding.UTF8;
-        try
-        {
-            string homePhonePattern = @"^\d{7}$";
-            Console.WriteLine("Ведіть свій домашній номер");
-            string homePhoneNumber = Console.ReadLine();
+    //1
+    //class City
+    //{
 
-            bool isHomePhoneValid = Regex.IsMatch(homePhoneNumber, homePhonePattern);
-
-            Console.WriteLine($"Домашній номер телефону валідний: {isHomePhoneValid}");
+    //    public string CityName { get; set; }
+    //    public string RegionName { get; set; }
+    //    public string CountryName { get; set; }
+    //    public int Population { get; set; }
+    //    public string PostalCode { get; set; }
+    //    public string PhoneCode { get; set; }
 
 
-            string mobilePhonePattern = @"^\+?\d{10,15}$";
-            Console.WriteLine("Ведіть  номер телефону");
-            string mobilePhoneNumber = Console.ReadLine();
-
-            bool isMobilePhoneValid = Regex.IsMatch(mobilePhoneNumber, mobilePhonePattern);
-            Console.WriteLine($"Мобільний номер телефону валідний: {isMobilePhoneValid}");
-
-
-            string emailPattern = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2}$";
-            Console.WriteLine("Ведіть свій email адрес");
-            string emailAddress = Console.ReadLine();
-
-            bool isEmailValid = Regex.IsMatch(emailAddress, emailPattern);
-            Console.WriteLine($"Email валідний: {isEmailValid}");
+    //    public City(string cityName, string regionName, string countryName, int population, string postalCode, string phoneCode)
+    //    {
+    //        CityName = cityName;
+    //        RegionName = regionName;
+    //        CountryName = countryName;
+    //        Population = population;
+    //        PostalCode = postalCode;
+    //        PhoneCode = phoneCode;
+    //    }
 
 
-            string fullNamePattern = @"^[\p{L}]{2,20}\s[\p{L}]{2,20}\s[\p{L}]{2,20}$";
-            Console.WriteLine("Ведіть своє ПІБ");
+    //    public void GetCityInfo()
+    //    {
+    //        Console.WriteLine($"Місто: {CityName}");
+    //        Console.WriteLine($"Регіон: {RegionName}");
+    //        Console.WriteLine($"Країна: {CountryName}");
+    //        Console.WriteLine($"Населення: {Population} жителів");
+    //        Console.WriteLine($"Поштовий індекс: {PostalCode}");
+    //        Console.WriteLine($"Телефонний код: {PhoneCode}");
+    //    }
+    //}
 
-            string fullName = Console.ReadLine();
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.OutputEncoding = Encoding.UTF8;
+    //        City kharkiv = new City("Харків", "Харківська область", "Україна", 1431000, "61000", "+38057");
+    //        kharkiv.GetCityInfo();
+    //        Console.WriteLine();
 
-            bool isFullNameValid = Regex.IsMatch(fullName, fullNamePattern);
-            Console.WriteLine($"ПІБ валідне: {isFullNameValid}");
-            Console.ReadKey();
-        }
-        catch (Exception ex) 
-        { Console.WriteLine(" Возникла ошибка пожалуйста соблюдайте инструкции"); }
-    }
+
+    //        City donetsk = new City("Донецьк", "Донецька область", "Україна", 950000, "83000", "+38062");
+    //        donetsk.GetCityInfo();
+    //        Console.WriteLine();
+
+
+    //        City zhytomyr = new City("Житомер", "Житомирська область", "Україна", 270000, "10000", "+38041");
+    //        zhytomyr.GetCityInfo();
+
+    //        Console.ReadLine();
+    //    }
+    //}
+
+
+
+
+
+    //2
+    //class Country
+    //{
+      
+    //    public string CountryName { get; set; }
+    //    public string ContinentName { get; set; }
+    //    public int Population { get; set; }
+    //    public string PhoneCode { get; set; }
+    //    public string CapitalName { get; set; }
+    //    public string[] CityNames { get; set; }
+
+        
+    //    public Country(string countryName, string continentName, int population, string phoneCode, string capitalName, string[] cityNames)
+    //    {
+    //        CountryName = countryName;
+    //        ContinentName = continentName;
+    //        Population = population;
+    //        PhoneCode = phoneCode;
+    //        CapitalName = capitalName;
+    //        CityNames = cityNames;
+    //    }
+
+        
+    //    public void GetCountryInfo()
+    //    {
+    //        Console.WriteLine($"Країна: {CountryName}");
+    //        Console.WriteLine($"Континент: {ContinentName}");
+    //        Console.WriteLine($"Населення: {Population} жителів");
+    //        Console.WriteLine($"Телефонний код: {PhoneCode}");
+    //        Console.WriteLine($"Столиця: {CapitalName}");
+    //        Console.WriteLine($"Міста: {string.Join(", ", CityNames)}");
+    //    }
+    //}
+
+    //class Program
+    //{
+    //    static void Main()
+    //    {
+    //        Console.OutputEncoding = Encoding.UTF8;
+    //        Country greece = new Country("Греція", "Європа", 10715549, "+30", "Афіни", new string[] { "Салоніки", "Патра", "Ларіса" });
+    //        greece.GetCountryInfo();
+
+
+    //        Country croatia = new Country("Хорватія", "Європа", 4105493, "+385", "Загреб", new string[] { "Спліт", "Рієка", "Осієк" });
+    //        croatia.GetCountryInfo();
+
+
+    //        Country mexico = new Country("Мексика", "Північна Америка", 126190788, "+52", "Мехіко", new string[] { "Гвадалахара", "Монтеррей", "Пуебла" });
+    //        mexico.GetCountryInfo();
+
+    //        Console.ReadLine();
+    //    }
+
+
+
+    //}
 }
