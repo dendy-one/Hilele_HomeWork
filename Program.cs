c8fc65a32d15e58d8918f735d7a9416f66ba5e3d
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-08-14 18:38:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,125 +1,166 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HomeWork10
+class Car
 {
-    //1
-    //class City
-    //{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    private double mileage;
 
-    //    public string CityName { get; set; }
-    //    public string RegionName { get; set; }
-    //    public string CountryName { get; set; }
-    //    public int Population { get; set; }
-    //    public string PostalCode { get; set; }
-    //    public string PhoneCode { get; set; }
+    public int Age
+    {
+        get
+        {
+            int currentYear = DateTime.Now.Year;
+            return currentYear - Year;
+        }
+    }
 
+    public double Mileage
+    {
+        get { return mileage; }
+        private set { mileage = value; }
+    }
 
-    //    public City(string cityName, string regionName, string countryName, int population, string postalCode, string phoneCode)
-    //    {
-    //        CityName = cityName;
-    //        RegionName = regionName;
-    //        CountryName = countryName;
-    //        Population = population;
-    //        PostalCode = postalCode;
-    //        PhoneCode = phoneCode;
-    //    }
+    public Car(string brand, string model, int year, double mileage)
+    {
+        Brand = brand;
+        Model = model;
+        Year = year;
+        Mileage = mileage;
+    }
 
+    public void Drive(double distance)
+    {
+        if (distance > 0)
+        {
+            Mileage += distance;
+            Console.WriteLine($"Пробіг автомобіля збільшено на {distance} км. Загальний пробіг: {Mileage} км.");
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення відстані. Пробіг не змінено.");
+        }
+    }
 
-    //    public void GetCityInfo()
-    //    {
-    //        Console.WriteLine($"Місто: {CityName}");
-    //        Console.WriteLine($"Регіон: {RegionName}");
-    //        Console.WriteLine($"Країна: {CountryName}");
-    //        Console.WriteLine($"Населення: {Population} жителів");
-    //        Console.WriteLine($"Поштовий індекс: {PostalCode}");
-    //        Console.WriteLine($"Телефонний код: {PhoneCode}");
-    //    }
-    //}
+    ~Car()
+    {
+        Console.WriteLine($"Об'єкт автомобіля {Brand} {Model} видалено.");
+    }
+}
 
-    //internal class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        Console.OutputEncoding = Encoding.UTF8;
-    //        City kharkiv = new City("Харків", "Харківська область", "Україна", 1431000, "61000", "+38057");
-    //        kharkiv.GetCityInfo();
-    //        Console.WriteLine();
+class ElectricCar : Car
+{
+    public double BatteryCapacity { get; set; }
 
+    public ElectricCar(string brand, string model, int year, double mileage, double batteryCapacity)
+        : base(brand, model, year, mileage)
+    {
+        BatteryCapacity = batteryCapacity;
+    }
 
-    //        City donetsk = new City("Донецьк", "Донецька область", "Україна", 950000, "83000", "+38062");
-    //        donetsk.GetCityInfo();
-    //        Console.WriteLine();
+    public void Charge(double amount)
+    {
+        if (amount > 0)
+        {
+            BatteryCapacity += amount;
+            Console.WriteLine($"Заряд батареї збільшено на {amount} кВт·год.");
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення заряду. Заряд батареї не змінено.");
+        }
+    }
 
+    public new void Drive(double distance)
+    {
+        if (distance > 0)
+        {
+            base.Drive(distance);
+            double energyConsumption = distance * 0.1;
+            BatteryCapacity -= energyConsumption;
 
-    //        City zhytomyr = new City("Житомер", "Житомирська область", "Україна", 270000, "10000", "+38041");
-    //        zhytomyr.GetCityInfo();
+          
+            if (BatteryCapacity < 0)
+            {
+                BatteryCapacity = 0;
+            }
 
-    //        Console.ReadLine();
-    //    }
-    //}
+            Console.WriteLine($"Рівень заряду батареї після поїздки: {BatteryCapacity:F2} кВт·год.");
+        }
+        else
+        {
+            Console.WriteLine("Некоректне значення відстані. Пробіг не змінено.");
+        }
+    }
+}
 
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            //данні звичайного авто
+            Console.WriteLine("Введіть інформацію про звичайний автомобіль:");
+            Console.Write("Марка: ");
+            string brand = Console.ReadLine();
+            Console.Write("Модель: ");
+            string model = Console.ReadLine();
+            Console.Write("Рік випуску: ");
+            int year = int.Parse(Console.ReadLine());
+            Console.Write("Пробіг (км): ");
+            double mileage = double.Parse(Console.ReadLine());
 
+            Car myCar = new Car(brand, model, year, mileage);
 
+            Console.WriteLine($"\nМарка: {myCar.Brand}, Модель: {myCar.Model}, Рік: {myCar.Year}, Пробіг: {myCar.Mileage} км, Вік: {myCar.Age} років");
 
-    //2
-    //class Country
-    //{
-      
-    //    public string CountryName { get; set; }
-    //    public string ContinentName { get; set; }
-    //    public int Population { get; set; }
-    //    public string PhoneCode { get; set; }
-    //    public string CapitalName { get; set; }
-    //    public string[] CityNames { get; set; }
+            //електро авто
+            Console.WriteLine("\nВведіть інформацію про електромобіль:");
+            Console.Write("Марка: ");
+            string eBrand = Console.ReadLine();
+            Console.Write("Модель: ");
+            string eModel = Console.ReadLine();
+            Console.Write("Рік випуску: ");
+            int eYear = int.Parse(Console.ReadLine());
+            Console.Write("Пробіг (км): ");
+            double eMileage = double.Parse(Console.ReadLine());
+            Console.Write("Ємність батареї (кВт·год): ");
+            double batteryCapacity = double.Parse(Console.ReadLine());
 
-        
-    //    public Country(string countryName, string continentName, int population, string phoneCode, string capitalName, string[] cityNames)
-    //    {
-    //        CountryName = countryName;
-    //        ContinentName = continentName;
-    //        Population = population;
-    //        PhoneCode = phoneCode;
-    //        CapitalName = capitalName;
-    //        CityNames = cityNames;
-    //    }
+            ElectricCar myElectricCar = new ElectricCar(eBrand, eModel, eYear, eMileage, batteryCapacity);
 
-        
-    //    public void GetCountryInfo()
-    //    {
-    //        Console.WriteLine($"Країна: {CountryName}");
-    //        Console.WriteLine($"Континент: {ContinentName}");
-    //        Console.WriteLine($"Населення: {Population} жителів");
-    //        Console.WriteLine($"Телефонний код: {PhoneCode}");
-    //        Console.WriteLine($"Столиця: {CapitalName}");
-    //        Console.WriteLine($"Міста: {string.Join(", ", CityNames)}");
-    //    }
-    //}
+            Console.WriteLine($"\nМарка: {myElectricCar.Brand}, Модель: {myElectricCar.Model}, Рік: {myElectricCar.Year}, Пробіг: {myElectricCar.Mileage} км, Ємність батареї: {myElectricCar.BatteryCapacity} кВт·год");
 
-    //class Program
-    //{
-    //    static void Main()
-    //    {
-    //        Console.OutputEncoding = Encoding.UTF8;
-    //        Country greece = new Country("Греція", "Європа", 10715549, "+30", "Афіни", new string[] { "Салоніки", "Патра", "Ларіса" });
-    //        greece.GetCountryInfo();
+            
+            Console.Write("\nВведіть відстань для поїздки на звичайному автомобілі (км): ");
+            double distance = double.Parse(Console.ReadLine());
+            myCar.Drive(distance);
 
+            Console.Write("\nВведіть відстань для поїздки на електромобілі (км): ");
+            double eDistance = double.Parse(Console.ReadLine());
+            myElectricCar.Drive(eDistance);
 
-    //        Country croatia = new Country("Хорватія", "Європа", 4105493, "+385", "Загреб", new string[] { "Спліт", "Рієка", "Осієк" });
-    //        croatia.GetCountryInfo();
+            Console.Write("\nВведіть кількість заряду для електромобіля (кВт·год): ");
+            double chargeAmount = double.Parse(Console.ReadLine());
+            myElectricCar.Charge(chargeAmount);
 
+            
+            Console.WriteLine($"\nПісля поїздки і зарядки:");
+            Console.WriteLine($"Звичайний автомобіль - Пробіг: {myCar.Mileage} км, Вік: {myCar.Age} років");
+            Console.WriteLine($"Електромобіль - Пробіг: {myElectricCar.Mileage} км, Ємність батареї: {myElectricCar.BatteryCapacity} кВт·год");
 
-    //        Country mexico = new Country("Мексика", "Північна Америка", 126190788, "+52", "Мехіко", new string[] { "Гвадалахара", "Монтеррей", "Пуебла" });
-    //        mexico.GetCountryInfo();
-
-    //        Console.ReadLine();
-    //    }
-
-
-
-    //}
+           
+            myElectricCar = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Помилка введення даних. Переконайтеся, що всі числові значення введені правильно.");
+        }
+    }
 }
