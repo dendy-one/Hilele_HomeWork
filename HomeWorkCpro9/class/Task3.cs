c54329b734ae9128d3cac703932249b5d5082764
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-11-08 08:03:39 +0100

    Add files via upload

---
+++ HomeWorkCpro9/class/Task3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Task3
+{
+    public static async Task RunAsync()
+    {
+        await DelayedAction();
+    }
+
+    static async Task DelayedAction()
+    {
+        await Task.Delay(2000);
+        Console.WriteLine("Затримана дія завершена.");
+    }
+}
