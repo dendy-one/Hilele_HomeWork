6e27f5e8fe01d71cd4b247190d78360b67de5c91
dendy-one <150953486+dendy-one@users.noreply.github.com>
2024-11-08 08:00:48 +0100

    Add files via upload

---
+++ TestProjectHillelHomeWorkcpro24/class/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace MyProject
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+
+        public int Subtract(int a, int b) => a - b;
+
+        public int Multiply(int a, int b) => a * b;
+
+        public double Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return (double)a / b;
+        }
+
+        public double Average(int[] numbers)
+        {
+            if (numbers.Length == 0) throw new InvalidOperationException("Array is empty");
+            return numbers.Average();
+        }
+
+        public int Max(int[] numbers)
+        {
+            if (numbers.Length == 0) throw new InvalidOperationException("Array is empty");
+            return numbers.Max();
+        }
+
+        public int[] Sort(int[] numbers) => numbers.OrderBy(n => n).ToArray();
+    }
+}
---
+++ TestProjectHillelHomeWorkcpro24/class/CalculatorTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Xunit;
+using TestProjectHillelHomeWorkcpro24;
+using MyProject;
+
+namespace TestProjectHillelHomeWorkcpro24
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectSum()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(5, calculator.Add(2, 3));
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectDifference()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(1, calculator.Subtract(3, 2));
+        }
+
+        [Fact]
+        public void Multiply_ReturnsCorrectProduct()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(6, calculator.Multiply(2, 3));
+        }
+
+        [Fact]
+        public void Divide_ReturnsCorrectQuotient()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(2, calculator.Divide(6, 3));
+        }
+
+        [Fact]
+        public void Divide_ByZero_ThrowsException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<DivideByZeroException>(() => calculator.Divide(6, 0));
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, 2)]
+        [InlineData(new[] { -1, -2, -3 }, -2)]
+        public void Average_CalculatesCorrectAverage(int[] numbers, double expected)
+        {
+            var calculator = new Calculator();
+            Assert.Equal(expected, calculator.Average(numbers));
+        }
+
+        [Fact]
+        public void Average_EmptyArray_ThrowsException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<InvalidOperationException>(() => calculator.Average(new int[] { }));
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, 3)]
+        [InlineData(new[] { -1, -2, -3 }, -1)]
+        [InlineData(new[] { 1 }, 1)]
+        public void Max_ReturnsLargestNumber(int[] numbers, int expected)
+        {
+            var calculator = new Calculator();
+            Assert.Equal(expected, calculator.Max(numbers));
+        }
+
+        [Fact]
+        public void Max_EmptyArray_ThrowsException()
+        {
+            var calculator = new Calculator();
+            Assert.Throws<InvalidOperationException>(() => calculator.Max(new int[] { }));
+        }
+
+        [Theory]
+        [InlineData(new[] { 3, 1, 2 }, new[] { 1, 2, 3 })]
+        [InlineData(new int[] { }, new int[] { })]
+        [InlineData(new[] { 1 }, new[] { 1 })]
+        public void Sort_ReturnsSortedArray(int[] numbers, int[] expected)
+        {
+            var calculator = new Calculator();
+            Assert.Equal(expected, calculator.Sort(numbers));
+        }
+    }
+}
